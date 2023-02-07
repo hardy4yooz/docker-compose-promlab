4f62eee80d4192a3f85b3150c4a55749b7e435ad
hardy4yooz <4044094+hardy4yooz@users.noreply.github.com>
2023-02-07 18:19:20 +0800

    feat: add consul service discovery and serivce demo.

---
+++ services/go-gin-demo2/example.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var requestCounter = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "requests_total",
+		Help: "Number of requests processed, partitioned by status code and HTTP method.",
+	},
+	[]string{"code", "method"},
+)
+
+func init() {
+	prometheus.MustRegister(requestCounter)
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/health", func(c *gin.Context) {
+		// Increase request count
+		requestCounter.With(prometheus.Labels{"code": "200", "method": "GET"}).Inc()
+		c.JSON(http.StatusOK, gin.H{
+			"Status": "passing",
+			"Output": "Everything is okay",
+		})
+	})
+
+	r.GET("/metrics", func(c *gin.Context) {
+		h := promhttp.Handler()
+		h.ServeHTTP(c.Writer, c.Request)
+	})
+
+	r.Run(":5002")
+
+}
